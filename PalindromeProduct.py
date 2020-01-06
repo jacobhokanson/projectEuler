1695040eb720505913a953665c2607dbb69b472c
unknown <jacob.hokanson9@gmail.com>
2020-01-06 12:30:29 -0800

    Improved test module & Problem 4

---
+++ PalindromeProduct.py
@@ -0,0 +1,43 @@
+# Project Euler Problem 4
+from mytc import Test
+
+def PalindromeProduct(n):
+    if n == 1:
+        return 9
+    else:
+        test_num = int('9' * n) ** 2
+        while True:
+            while not testPalindrome(test_num):
+                test_num -= 1
+            print("trying", test_num, "- checking for factors...")
+            for fac1 in range(int('9' * n), int('9' * (n - 1)), -1):
+                if test_num % fac1 == 0:
+                    print(" -", fac1, "is a factor...it's complement is", test_num // fac1)
+                    if len(str(test_num // fac1)) == n: # fac2
+                        return test_num
+            test_num -= 1
+    return None
+
+def testPalindrome(n):
+    n = str(n)
+    return n[:len(n)//2] == n[-(len(n)//2):][::-1]
+
+tc = {
+    1: 9,
+    2: 9009,
+    3: 906609,
+}
+
+# pa = {
+#     "lol": True,
+#     "lool": True,
+#     "this": False,
+#     "operepo": True,
+#     111: True,
+#     121: True,
+#     122: False
+# }
+
+# Test(pa, testPalindrome)
+
+Test(tc, PalindromeProduct)
--- mytc.py
+++ mytc.py
@@ -1,6 +1,12 @@
 # myTestCase
 
 def Test(cases, fn): # Dict
+    results = [] # List[Tuple] # (Input, Output, Expected)
     for (test, value) in cases.items():
-        print("fn(" + str(test) + ")", "is", result := fn(test), "-- should be", value)
-        assert result == value
+        results.append((fn.__name__ + "(" + str(test) + ")", fn(test), value))
+        # print("fn(" + str(test) + ") is", result := fn(test), "-- should be", value)
+        # assert result == value
+    print("\n" + "-" * 30, "MYTC RESULTS", "-" * 30)
+    for case in results:
+        print(" -", case[0], "returned", case[1], "expected", case[2])
+        
