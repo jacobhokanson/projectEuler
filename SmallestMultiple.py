16fcd7d733960ac36bd0b12727ce0eff6ab1d87a
unknown <jacob.hokanson9@gmail.com>
2020-01-08 12:13:47 -0800

    Problem 5, improved test printing

---
+++ SmallestMultiple.py
@@ -0,0 +1,45 @@
+# Project Euler Problem 5
+from mytc import Test
+
+def sm(n):
+    if n == 1:
+        return 1
+    prev = sm(n-1)
+    for i in range(prev, prev*n+1, prev):
+        if i % n == 0:
+            return i
+    
+tc = {
+    1: 1,
+    2: 2,
+    3: 6,
+    4: 12,
+    5: 60,
+    7: 420,
+    10: 2520,
+    13: 360360,
+    20: 232792560,
+}
+
+Test(tc, sm)
+
+# ARCHIVES
+
+# for i in count(1):
+#     print(c, "-", i, "times sm(" + str(n-1) + ")")
+#     if i * sm(n-1) % n == 0:
+#         return i * sm(n-1)
+
+# def SmallestMultiple(n):
+#     for i in range(1, factorial(n)+1):
+#         if not [p for p in range(1, n+1) if not i % p == 0]: # listcomp gives any nums from 1 to n that do not divide
+#             return i
+# 
+#     [i for i in range(1, f(o)+1) if [p for p in range(1, o+1) if not i % p == 0] == []]
+
+# def SmallestMultiple(n): # Returns smallest positive integer evenly divisible by all numbers from 1 to n
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 2
+#     return (SmallestMultiple(n - 1) * n) / 
--- mytc.py
+++ mytc.py
@@ -1,12 +1,21 @@
 # myTestCase
 
+m = ("PASS", "FAIL")
+
 def Test(cases, fn): # Dict
     results = [] # List[Tuple] # (Input, Output, Expected)
     for (test, value) in cases.items():
         results.append((fn.__name__ + "(" + str(test) + ")", fn(test), value))
         # print("fn(" + str(test) + ") is", result := fn(test), "-- should be", value)
         # assert result == value
-    print("\n" + "-" * 30, "MYTC RESULTS", "-" * 30)
+    print(f'{" MYTC RESULTS ":-^74}')
+
+    # print("\n" + "-" * 30, "MYTC RESULTS", "-" * 30)
     for case in results:
-        print(" -", case[0], "returned", case[1], "expected", case[2])
-        
+        print(f'{f" - {case[0]} returned {case[1]} expected {case[2]} ":.<69}', "PASS" if case[1] == case[2] else "FAIL")
+
+        # WOW learning this was a pain
+        # print(" -", case[0], "returned", case[1], "expected", case[2], "--", "PASS" if case[1] == case[2] else "FAIL")
+        # print(f' - {case[0]} returned {case[1]} expected {case[2]} --', f'{"PASS" if case[1] == case[2] else "FAIL"}')
+        # print(f'{" - {case[0]} returned {case[1]} expected {case[2]} -- {'PASS' if case[1] == case[2] else 'FAIL'}":<12}')
+        # print(f'{f" - {case[0]} returned {case[1]} expected {case[2]} -- {m[0] if case[1] == case[2] else m[1]}":>50}')
